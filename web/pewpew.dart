7f7bf3d75597550fe052684df9222c47e2ad4b4e
Graham MacDonald <grahamamacdonald@gmail.com>
2013-11-24 22:16:15 +0000

    WIP for further work at work...

--- web/pewpew.dart
+++ web/pewpew.dart
@@ -5,14 +5,33 @@ import 'package:game_loop/game_loop_html.dart';
 import 'package:pewpew/gamewrapper.dart';
 
 
+const int numRows = 5;
+const int numCols = 11;
+
+List<Invader> invaders = new List<Invader>();
+
 main() {
-  Element gameContainer = querySelector("#gameContainer");
-  initGameWrapper(gameContainer, init);
+  initGameWrapper(querySelector("#gameContainer"), init);
 }
 
 init() {
   entities.add(new Ship(100, 100));
-  entities.add(new Invader(50, 100));
+
+  int invaderStartX = 20;
+  int invaderStartY = 40;
+  int invaderGapX = 30;
+  int invaderGapY = 35;
+  for (int y = 0; y < numRows; y++) {
+    for (int x = 0; x < numCols; x++) {
+      Invader invader = new Invader(
+          invaderStartX + invaderGapX * x,
+          invaderStartY + invaderGapY * y);
+      invaders.add(invader);
+      entities.add(invader);
+    }
+  }
+
+  entities.add(new InvaderController(invaders));
 }
 
 
@@ -41,22 +60,34 @@ class Ship extends Entity {
 
 class Invader extends Entity {
   Texture _tex;
-  int _x, _y;
+  int x, y;
 
-  Invader(this._x, this._y) {
+  Invader(this.x, this.y) {
     _tex = textures.add(#invader, "sprites.png", 0, 0, 16, 16);
   }
 
   update(UpdateContext u) {
-    if (u.gameLoop.keyboard.isDown(Keyboard.LEFT)) {
-      _x -= 1;
-    }
-    else if (u.gameLoop.keyboard.isDown(Keyboard.RIGHT)) {
-      _x += 1;
-    }
   }
 
   render(RenderContext r) {
-    _tex.draw(r, _x, _y);
+    _tex.draw(r, x, y);
+  }
+}
+
+class InvaderController extends Entity {
+  List<Invader> _invaders;
+  InvaderController(this._invaders);
+
+  int _updateIdx = 0;
+
+  update(UpdateContext u) {
+    int row = _updateIdx % numRows;
+    int firstInvader = row * numCols;
+    int lastInvader = firstInvader + numCols;
+    for (int i = firstInvader; i < lastInvader; i++) {
+      _invaders[i].x += 1;
+    }
+
+    _updateIdx++;
   }
 }
